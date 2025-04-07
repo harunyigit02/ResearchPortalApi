ef88ccbe5a97050b19b4277df8c51d55fbfdd816
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-07 23:21:00 +0300

    Article File column added

---
+++ FinalProjectWebApi/Migrations/20250407200112_AlterContentColumnToBytea.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProjectWebApi.Migrations
+{
+    public partial class AlterContentColumnToBytea : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Content",
+                table: "Articles",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Content",
+                table: "Articles");
+        }
+    }
+}
