046cafbaf5f01461f89ad99c7fe509d00ddbadf3
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-07 23:20:42 +0300

    Question Multi Analyze endpoint Added

---
+++ FinalProjectWebApi/Entities/Abstract/FilterAnalyzeQuestionsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinalProjectWebApi.Entities.Abstract
+{
+    public class FilterAnalyzeQuestionsDto
+    {
+
+        public List<int> OptionIds { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
