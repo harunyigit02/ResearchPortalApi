e120930056e5fc3d26e886a2b47ade9a7f18f891
Harun Yiğit <f211220029@ktun.edu.tr>
2025-04-11 00:06:10 +0300

    Refresh Token method added

--- FinalProjectWebApi/Entities/Concrete/User.cs
+++ FinalProjectWebApi/Entities/Concrete/User.cs
@@ -11,6 +11,8 @@
         public byte[] PasswordSalt { get; set; }
 
         public string Role { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
         public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
     }
 }
