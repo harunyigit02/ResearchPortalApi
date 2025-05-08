977f44ce8724d0e75c6e6f2c204a680d503d0091
Harun Yiğit <f211220029@ktun.edu.tr>
2025-05-08 15:25:43 +0300

    refactor: removed refresh token methods and implementations

--- FinalProjectWebApi/Entities/Concrete/User.cs
+++ FinalProjectWebApi/Entities/Concrete/User.cs
@@ -11,8 +11,7 @@
         public byte[] PasswordSalt { get; set; }
 
         public string Role { get; set; }
-        public string RefreshToken { get; set; }
-        public DateTime RefreshTokenExpiryTime { get; set; }
+        
         public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
     }
 }
